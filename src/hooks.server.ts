524e7175d17d86ddd00aadfdbdf54a5866564b67
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2023-02-27 20:02:46 +0100

    feat: add auth to GitHub

---
+++ src/hooks.server.ts
@@ -0,0 +1,25 @@
+import { SvelteKitAuth } from '@auth/sveltekit';
+import GitHub from '@auth/core/providers/github';
+import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+
+export const handle = SvelteKitAuth({
+	providers: [
+		GitHub({
+			clientId: GITHUB_ID,
+			clientSecret: GITHUB_SECRET,
+			authorization: { params: { scope: 'notifications' } }
+		}) as any
+	],
+	callbacks: {
+		async jwt({ token, account }) {
+			if (account) {
+				token.accessToken = account.access_token;
+			}
+			return token;
+		},
+		async session({ session, token }) {
+			session.accessToken = token.accessToken;
+			return session;
+		}
+	}
+});
