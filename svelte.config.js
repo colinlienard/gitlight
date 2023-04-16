7da50cf2e3cd6ece298ae2b8441f6a2cccf1b58b
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2023-04-16 10:46:41 +0200

    fix: remove fallback

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ fallback: 'index.html' }),
+		adapter: adapter(),
 		alias: {
 			'~': './src'
 		}
