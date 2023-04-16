a75270fedbeef0d889a1f7b10c7c307dad1ccc06
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2023-04-16 12:10:47 +0200

    fix: vercel build command

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import staticAdapter from '@sveltejs/adapter-static';
+import vercelAdapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter:
+			process.env.APP_ENV === 'vercel'
+				? vercelAdapter()
+				: staticAdapter({ fallback: 'index.html' }),
 		alias: {
 			'~': './src'
 		}
