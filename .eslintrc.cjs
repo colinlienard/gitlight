797c249e9cf8333f45a4070233dcc86bc7072bdc
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2023-04-21 18:18:52 +0200

    bump

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -16,5 +16,9 @@ module.exports = {
 		browser: true,
 		es2017: true,
 		node: true
+	},
+	rules: {
+		'@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
+		'no-console': 'warn'
 	}
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
 	css: {
 		preprocessorOptions: {
 			scss: {
