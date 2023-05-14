46139be7b9d6b31fc1726dd3c431c2991a57622e
QuiiBz <tom.lienrd@gmail.com>
2023-05-14 11:28:54 +0200

    chore: lint & format Rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,79 +1,78 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
-use tauri::{Manager, SystemTray, SystemTrayEvent, SystemTrayMenuItem, CustomMenuItem, SystemTrayMenu};
+use tauri::{
+    CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+};
 
 #[tauri::command]
 fn update_tray(app_handle: tauri::AppHandle, title: String, description: String) {
-  let tray_handle = app_handle.tray_handle();
-  #[cfg(target_os= "macos")]
-  tray_handle.set_title(&title).unwrap();
-  tray_handle.get_item("text").set_title(description).unwrap();
+    let tray_handle = app_handle.tray_handle();
+    #[cfg(target_os = "macos")]
+    tray_handle.set_title(&title).unwrap();
+    tray_handle.get_item("text").set_title(description).unwrap();
 }
 
 fn main() {
-  tauri_plugin_deep_link::prepare("de.fabianlars.deep-link-test");
+    tauri_plugin_deep_link::prepare("de.fabianlars.deep-link-test");
 
-  let title = CustomMenuItem::new("title".to_string(), "GitHub notifications").disabled();
-  let text = CustomMenuItem::new("text".to_string(), "1 pinned • 2 unread");
-  let focus = CustomMenuItem::new("focus".to_string(), "Dashboard");
-  let quit = CustomMenuItem::new("quit".to_string(), "Quit");
-  let tray_menu = SystemTrayMenu::new()
-    .add_item(title)
-    .add_item(text)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(focus)
-    .add_native_item(SystemTrayMenuItem::Separator)
-    .add_item(quit);
-  let tray = SystemTray::new().with_menu(tray_menu);
+    let title = CustomMenuItem::new("title".to_string(), "GitHub notifications").disabled();
+    let text = CustomMenuItem::new("text".to_string(), "1 pinned • 2 unread");
+    let focus = CustomMenuItem::new("focus".to_string(), "Dashboard");
+    let quit = CustomMenuItem::new("quit".to_string(), "Quit");
+    let tray_menu = SystemTrayMenu::new()
+        .add_item(title)
+        .add_item(text)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(focus)
+        .add_native_item(SystemTrayMenuItem::Separator)
+        .add_item(quit);
+    let tray = SystemTray::new().with_menu(tray_menu);
 
-  tauri::Builder::default()
-    .setup(|app| {
-      let handle = app.handle();
-      tauri_plugin_deep_link::register("gitlight", move |request| {
-        handle.emit_all("scheme-request", request).unwrap();
-      })
-      .unwrap();
+    tauri::Builder::default()
+        .setup(|app| {
+            let handle = app.handle();
+            tauri_plugin_deep_link::register("gitlight", move |request| {
+                handle.emit_all("scheme-request", request).unwrap();
+            })
+            .unwrap();
 
-      #[cfg(not(target_os = "macos"))]
-      if let Some(url) = std::env::args().nth(1) {
-        app.emit_all("scheme-request", url).unwrap();
-      }
+            #[cfg(not(target_os = "macos"))]
+            if let Some(url) = std::env::args().nth(1) {
+                app.emit_all("scheme-request", url).unwrap();
+            }
 
-      Ok(())
-    })
-    .system_tray(tray)
-    .on_system_tray_event(|app, event| match event {
-      SystemTrayEvent::MenuItemClick { id, .. } => {
-        match id.as_str() {
-          "text" | "focus" => {
-            let window = app.get_window("main").unwrap();
-            window.set_focus().unwrap();
-          }
-          "quit" => {
-            std::process::exit(0);
-          }
-          _ => {}
-        }
-      }
-      _ => {}
-    })
-    .on_window_event(|event| match event.event() {
-      tauri::WindowEvent::CloseRequested { api, .. } => {
-        event.window().hide().unwrap();
-        api.prevent_close();
-      }
-      _ => {}
-    })
-    .invoke_handler(tauri::generate_handler![update_tray])
-    .build(tauri::generate_context!())
-    .expect("error while running tauri application")
-    .run(|_app_handle, event| match event {
-      tauri::RunEvent::ExitRequested { api, .. } => {
-        api.prevent_exit();
-      }
-      _ => {}
-    });
+            Ok(())
+        })
+        .system_tray(tray)
+        .on_system_tray_event(|app, event| {
+            if let SystemTrayEvent::MenuItemClick { id, .. } = event {
+                match id.as_str() {
+                    "text" | "focus" => {
+                        let window = app.get_window("main").unwrap();
+                        window.set_focus().unwrap();
+                    }
+                    "quit" => {
+                        std::process::exit(0);
+                    }
+                    _ => {}
+                }
+            }
+        })
+        .on_window_event(|event| {
+            if let tauri::WindowEvent::CloseRequested { api, .. } = event.event() {
+                event.window().hide().unwrap();
+                api.prevent_close();
+            }
+        })
+        .invoke_handler(tauri::generate_handler![update_tray])
+        .build(tauri::generate_context!())
+        .expect("error while running tauri application")
+        .run(|_app_handle, event| {
+            if let tauri::RunEvent::ExitRequested { api, .. } = event {
+                api.prevent_exit();
+            }
+        });
 }
