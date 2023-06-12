a341698c93c068059c6558418ff13fb293d05980
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2023-06-12 20:54:08 +0200

    improve

--- src/lib/helpers/removeMarkdownSymbols.ts
+++ src/lib/helpers/removeMarkdownSymbols.ts
@@ -2,10 +2,10 @@ const markdownSymbols: Array<[RegExp, string]> = [
 	[/#+\s/g, ''], // Remove headers (e.g., # Header)
 	[/(\*{1,2})(.*?)\1/g, '$2'], // Remove emphasis and bold (e.g., *emphasis* or **bold**)
 	[/~~(.*?)~~/g, '$1'], // Remove strikethrough (e.g., ~~strikethrough~~)
-	[/\[(.*?)\]\((.*?)\)/g, '$1 ($2)'], // Remove links (e.g., [link](url))
+	[/\[(.*?)\]\((.*?)\)/g, '$1'], // Remove links (e.g., [link](url))
 	[/\n- (.*)/g, '$1'], // Remove unordered list (e.g., - Item)
 	[/\n\d+\. (.*)/g, '$1'], // Remove ordered list (e.g., 1. Item)
-	[/\n> (.*)/g, '$1'], // Remove blockquotes (e.g., > Quote)
+	[/^>\s(.*)$/gm, '$1'], // Remove blockquotes (e.g., > Quote)
 	[/\[vc\]: #[\w=:]+/g, ''] // Remove vercel comment beginning
 ];
 
