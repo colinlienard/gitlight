6758cdec9bb022b770a93f946a7b1ed3f5d049e6
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2023-06-25 21:25:40 +0200

    feat(ci): support for arm64 systems

--- src/routes/version/[target]/[version]/+server.ts
+++ src/routes/version/[target]/[version]/+server.ts
@@ -1,49 +1,26 @@
 import type { GithubRelease } from '~/lib/types/github-types.js';
 
-type Target = 'linux' | 'windows' | 'darwin';
-
-const targetExtensions: Record<Target, string> = {
-	linux: '.AppImage.tar.gz',
-	windows: '.msi.zip',
-	darwin: '.app.tar.gz'
-};
-
 export async function GET({ params }) {
 	try {
-		const target = params.target as Target;
 		const { version } = params;
 
 		// Get latest release from Github
 		const response = await fetch('https://api.github.com/repos/colinlienard/gitlight/releases');
 		const data = (await response.json()) as GithubRelease[];
-		const { assets, published_at, body, tag_name } = data[0];
+		const { assets, tag_name } = data[0];
 		const latestVersion = tag_name.split('v')[1];
 
 		if (version === latestVersion) throw new Error();
 
-		const extension = targetExtensions[target];
+		// Get latest.json
+		const json = assets.find(({ name }) => name === 'latest.json');
 
-		if (!extension) throw new Error();
+		if (!json) throw new Error();
 
-		// Get the asset and its signature file
-		const updaterAsset = assets.find(({ name }) => name.endsWith(extension));
-		const signatureAsset = assets.find(({ name }) => name.endsWith(`${extension}.sig`));
-
-		if (!updaterAsset || !signatureAsset) throw new Error();
-
-		// Get the signature from the .sig file
-		const signatureResponse = await fetch(signatureAsset.browser_download_url);
-		const signature = await signatureResponse.text();
-
-		const returnValue = {
-			url: updaterAsset.browser_download_url,
-			version: latestVersion,
-			notes: body,
-			pub_date: published_at,
-			signature
-		};
-
-		return new Response(JSON.stringify(returnValue), { status: 200 });
+		// Return latest.json content
+		const jsonResponse = await fetch(json.browser_download_url);
+		const jsonData = await jsonResponse.text();
+		return new Response(jsonData, { status: 200 });
 	} catch {
 		return new Response(null, { status: 204 });
 	}
