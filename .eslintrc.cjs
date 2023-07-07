fcec0ce087ea6975fc985fe204a3835ad8f46da4
Colin Lienard <colin.lienard87@gmail.com>
2023-07-07 17:43:21 +0200

    sort import

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
 	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	plugins: ['svelte3', '@typescript-eslint', 'import'],
 	ignorePatterns: ['*.cjs'],
 	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
 	settings: {
@@ -19,6 +19,18 @@ module.exports = {
 	},
 	rules: {
 		'@typescript-eslint/no-unused-vars': [1, { argsIgnorePattern: '^_' }],
-		'no-console': ['warn', { allow: ['error'] }]
+		'no-console': ['warn', { allow: ['error'] }],
+		'import/order': [
+			'warn',
+			{
+				groups: ['external', 'unknown', 'internal', 'sibling', 'parent'],
+				pathGroups: [
+					{ pattern: '$.*', group: 'unknown' },
+					{ pattern: '~/lib/*', group: 'internal' }
+				],
+				alphabetize: { order: 'asc', caseInsensitive: true },
+				'newlines-between': 'never'
+			}
+		]
 	}
 };
