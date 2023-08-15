75b73a725904f7ea528b199d152fbd221e8db641
Olafur Waage <olafurw@gmail.com>
2023-08-15 19:33:38 +0200

    Linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,12 @@
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'prettier'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
 	plugins: ['@typescript-eslint', 'import'],
 	ignorePatterns: ['*.cjs'],
 	parserOptions: {
