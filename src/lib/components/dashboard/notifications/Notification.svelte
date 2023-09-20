14aacda6d943b3d319d8aeadacb2c146a9ccde36
Colin Lienard <colin.lienard87@gmail.com>
2023-09-20 21:11:40 +0200

    changes

--- src/lib/components/dashboard/notifications/Notification.svelte
+++ src/lib/components/dashboard/notifications/Notification.svelte
@@ -63,14 +63,13 @@
 		if (!isTrayApp && window.__TAURI__) {
 			unlisten = await listen<{ key: ToggleKey }>(`notification-toggle:${id}`, (event) => {
 				handleToggle(event.payload.key)();
+				unlisten();
 			});
 		}
 	});
 
 	onDestroy(() => {
-		if (!isTrayApp && browser && window.__TAURI__) {
-			unlisten();
-		}
+		unlisten();
 	});
 
 	function handleHoverActions(event: MouseEvent) {
