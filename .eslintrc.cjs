6ec0adc877eda07d8322839aa4883dcb0c66d5fe
Colin Lienard <colin.lienard87@gmail.com>
2024-05-01 18:27:58 +0200

    chore: bump deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,20 @@
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:svelte/recommended',
 		'prettier'
 	],
+	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint', 'import'],
-	ignorePatterns: ['*.cjs', 'svelte.config.js'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		parser: '@typescript-eslint/parser',
-		tsconfigRootDir: __dirname,
-		project: ['./tsconfig.json'],
 		extraFileExtensions: ['.svelte']
 	},
+	ignorePatterns: ['*.cjs', 'svelte.config.js'],
 	env: {
 		browser: true,
 		es2017: true,
@@ -25,7 +23,10 @@ module.exports = {
 	overrides: [
 		{
 			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser'
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
 		}
 	],
 	rules: {
